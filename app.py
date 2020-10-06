5dc530fe794d41605dd7240d281a75ae4b2b7e07
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 16:00:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
