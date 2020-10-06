ce7d58dc9f8a4637250d9802ddb80a9d12002335
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 20:17:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ core = Blueprint(
     static_folder='static'
 )
 
+@home_bp.route('/', methods=['GET'])
+def home():
+    """Homepage."""
+    products = fetch_products(app)
+    return render_template(
+        'index.jinja2',
+        title='Flask blog ',
+        subtitle='Alex soulis.',
+        template='home-template',
+        products=products
+    )
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
