b3e51df4e54753bd5843bb178e5b6a8ff2cbedf4
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 16:51:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import os
 import json
+from flask import current_app as app
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return render_template("core/base.html")
+    return render_template("core/base")
 
-@app.route('/index')
-def index():
-    return render_template("core/index.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
