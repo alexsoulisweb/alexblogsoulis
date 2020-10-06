6ec4a102a7fededcb04daf5c99e7344b299437c5
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 15:46:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
-from blog import app
+from flask import app
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("base.html")
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
