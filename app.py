49d49d6326a959fba1e466422a65dfaee66af54b
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 19:44:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ def index():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
 
 from views import *
