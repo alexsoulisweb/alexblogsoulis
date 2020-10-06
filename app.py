f8ed099c924c4ab2d3e985cb752d26137297f80b
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 14:57:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from blog import app
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True
+            debug=True)
