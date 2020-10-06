fcc943f1c84e533d84f688c786c428382fa531be
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 16:08:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
 from flask import Flask,render_template,request
+from flask import render_template,request,Blueprint
+from blog.models import BlogPost
 
-app = Flask(__name__)
+core = Blueprint('core',__name__)
 
-@app.route('/index')
+@core.route('/')
 def index():
-    return render_template("index.html")
+    
+    page = request.args.get('page', 1, type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=10)
+    return render_template('index.html',blog_posts=blog_posts)
+
+@core.route('/info')
+def info():
+    return render_template('info.html')
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
