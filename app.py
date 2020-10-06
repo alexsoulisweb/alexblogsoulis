7a177e84d68e33853a809a21c0ee5cefe79bb420
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 19:53:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask
+from flask import Blueprint
+from flask import current_app as app
 
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template("index.html")
+# Blueprint Configuration
+home_bp = Blueprint(
+    'home_bp', __name__,
+    template_folder='templates',
+    static_folder='static'
+)
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
-
-from views import *
+    app.run(debug=True)
