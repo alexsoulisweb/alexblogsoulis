8a9a95131943da40b015fde0b811655ed10e41bc
alexsoulisweb <60231375+alexsoulisweb@users.noreply.github.com>
2020-10-06 20:18:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from flask import current_app as app
 
 
-core = Blueprint(
+home_bp = Blueprint(
     'home_bp', __name__,
     template_folder='templates',
     static_folder='static'
